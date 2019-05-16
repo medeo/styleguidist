2d7333e9e9fdc3236efcc33805aad31e7f62b717
Chazz <charles@medeo-health.com>
2019-05-16 16:50:03 +0200

    chore(Span): add inline comment

--- src/components/Span.js
+++ src/components/Span.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import styled, { css } from 'styled-components';
 import { lighten, mix, readableColor } from 'polished';
 
+// spqn is inline
 const Span = styled.span`
 	display: inline-block;
 	color: ${p => p.theme[p.color]};

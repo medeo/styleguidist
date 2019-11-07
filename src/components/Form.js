f834902a3069533ef917ea3ebb4ab6d245b22b9f
Charles <charles@medeo-health.com>
2019-11-07 16:16:34 +0100

    v7.0.2

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 const Form = styled.form``
 Form.Row = styled.div`
 	display: grid;
-	grid-template-columns: repeat(auto-fit, minmax(10rem, 1fr));
+	//grid-template-columns: repeat(auto-fit, minmax(10rem, 1fr));
 	grid-column-gap: ${p => p.theme.spacing.medium};
 	margin-bottom: ${p => p.theme.spacing.medium};
 `
